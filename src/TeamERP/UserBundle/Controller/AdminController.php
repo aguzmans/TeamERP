9b945c21fbb2772f468eec95405be6fe167c6347
aguzmans <abel.aguzmans@gmail.com>
2015-02-02 17:58:57 +0200

    administration of users; create new, list users

--- src/TeamERP/UserBundle/Controller/AdminController.php
+++ src/TeamERP/UserBundle/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace TeamERP\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Description of AdminController
@@ -17,4 +18,34 @@ class AdminController extends Controller
         $users = $userManager->findUsers();
         return $this->render('TeamERPUserBundle:Admin:admin.html.twig',array('users' =>$users));
     }   
+    public function editUserAction($id){
+
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserByUsername($id);
+
+        $form = $this->container->get('fos_user.profile.form');
+        $formHandler = $this->container->get('fos_user.profile.form.handler');
+
+        $process = $formHandler->process($user);
+        if ($process) {
+            $user->upload();
+            $this->setFlash('fos_user_success', 'profile.flash.updated');
+
+            return new RedirectResponse($this->container->get('router')->generate('fos_user_profile_show'));
+        }
+
+        return $this->container->get('templating')->renderResponse(
+        'FOSUserBundle:Profile:edit.html.'.$this->container->getParameter('fos_user.template.engine'),
+        array('form' => $form->createView())
+        );
+    }
+    public function deleteUserAction($aUser){
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this     section.');
+        }
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserByUsername($aUser);    
+        $userManager->deleteUser($user);
+    }
 }
